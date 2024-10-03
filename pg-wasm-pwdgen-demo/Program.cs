dd36a5142d306326be9bc76352e396748a00f494
drodriguez <drodriguez@esdelogica.es>
2024-10-03 22:12:43 +0200

    password-genie\pg-wasm-pwdgen-demo - Proof of concept of a Blazor Web Assembly Standalone web (#8).

---
+++ pg-wasm-pwdgen-demo/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PG.Logic.Passwords.Generators;
+using PG.Shared.Services;
+using pg_wasm_pwdgen_demo;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+		.AddSingleton<PasswordGeneratorFactory>()
+		.AddTransient<RandomService>()
+		.BuildServiceProvider();
+
+await builder.Build().RunAsync();
