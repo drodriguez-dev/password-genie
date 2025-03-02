9f874a12dfdd96e01d50d22aec0cbc0cac046996
drodriguez <drodriguez@esdelogica.es>
2025-03-02 09:33:42 +0100

    pg-wasm-pwdgen\Program - Move 'Program' into a named namespace. Types should be defined in named namespaces csharpsquid:S3903

--- pg-wasm-pwdgen/Program.cs
+++ pg-wasm-pwdgen/Program.cs
@@ -7,24 +7,18 @@ using PG.Logic.Passwords.Loaders;
 using PG.Shared.Services;
 using PG.Wasm.PasswordGenerator;
 
-internal class Program
-{
-	private static async Task Main(string[] args)
-	{
-		var builder = WebAssemblyHostBuilder.CreateDefault(args);
-		builder.RootComponents.Add<App>("#app");
-		builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-		builder.Services
-			.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-			.AddBlazorBootstrap()
-			.AddSingleton<PasswordGeneratorFactory>()
-			.AddSingleton<WordExtractorFactory>()
-			.AddSingleton<IDictionaryLoaderFactory, DictionaryLoaderFactory>()
-			.AddSingleton<IDictionariesDataFactory, DictionariesDataFactory>()
-			.AddTransient<RandomService>()
-			.BuildServiceProvider();
+builder.Services
+	.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+	.AddBlazorBootstrap()
+	.AddSingleton<PasswordGeneratorFactory>()
+	.AddSingleton<WordExtractorFactory>()
+	.AddSingleton<IDictionaryLoaderFactory, DictionaryLoaderFactory>()
+	.AddSingleton<IDictionariesDataFactory, DictionariesDataFactory>()
+	.AddTransient<RandomService>()
+	.BuildServiceProvider();
 
-		await builder.Build().RunAsync();
-	}
-}
+await builder.Build().RunAsync();
