cb572ad7bdee3fa3f154b2740a68437b10b276c1
drodriguez <drodriguez@esdelogica.es>
2025-03-22 15:44:01 +0100

    pg-wasm-pwdgen\Layout\NavigationMenu - Show the version in the top bar.

---
+++ pg-shared/Services/ApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace PG.Shared.Services
+{
+	public class ApplicationService
+	{
+		public string GetVersion()
+		{
+			var assembly = Assembly.GetEntryAssembly();
+			var versionAttribute = assembly?.GetCustomAttribute<AssemblyFileVersionAttribute>();
+			return versionAttribute?.Version ?? "Version not found";
+		}
+	}
+}
