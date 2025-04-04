23a03b6320f1d7263eb68a0eab69295f694de5fe
drodriguez <drodriguez@esdelogica.es>
2025-04-04 19:09:18 +0200

    pg-wasm-pwdgen\Pages\Password\Dictionary - New UI for the dictionary generator options (#31).

--- pg-wasm-pwdgen/Components/Passwords/Models/PasswordOptions.cs
+++ pg-wasm-pwdgen/Components/Passwords/Models/PasswordOptions.cs
@@ -13,16 +13,39 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 		private bool includeSeparatorSymbols = true;
 		private bool includeMarkSymbols = true;
 		private string customSpecialCharacters = string.Empty;
-		private bool isRandomStrokeOrder = true;
-		private bool isAlternatingStrokeOrder = false;
+		private bool isRandomStrokeOrder = false;
+		private bool isAlternatingStrokeOrder = true;
 		private bool isAlternatingWordKeystroke = false;
 		private bool isOnlyLeftKeystrokes = false;
 		private bool isOnlyRightKeystrokes = false;
+		private int numberOfWords = 2;
+		private int averageWordLength = 7;
+		private int depthLevel = 4;
+		private int minimumLength = 1;
 
+		#region Dictionary specific options
+		[Required(ErrorMessage = "Number of words is required")]
+		[Range(1, 30, ErrorMessage = "Number of words should be between 1 and 30.")]
+		public int NumberOfWords { get => numberOfWords; set { numberOfWords = value; OnPropertyChanged(nameof(NumberOfWords)); } }
+
+		[Required(ErrorMessage = "Average word length is required")]
+		[Range(4, 20, ErrorMessage = "Average word length should be between 4 and 20.")]
+		public int AverageWordLength { get => averageWordLength; set { averageWordLength = value; OnPropertyChanged(nameof(AverageWordLength)); } }
+
+		// Depth level must be lower than the average word length
+		[Required(ErrorMessage = "Depth level is required")]
+		[Range(2, 20, ErrorMessage = "Depth level should be between 2 and 20.")]
+		[CustomValidation(typeof(PasswordOptions), nameof(ValidateDepthLowerThanAverage), ErrorMessage = "Depth level must be lower than the average word length")]
+		public int DepthLevel { get => depthLevel; set { depthLevel = value; OnPropertyChanged(nameof(DepthLevel)); } }
+		#endregion
+
+		#region Random specific options
 		[Required(ErrorMessage = "Number of letters is required")]
 		[Range(0, 100, ErrorMessage = "Number of letters should be between 0 and 100.")]
 		public int NumberOfLetters { get => numberOfLetters; set { numberOfLetters = value; OnPropertyChanged(nameof(NumberOfLetters)); } }
+		#endregion
 
+		#region Common options
 		[Required(ErrorMessage = "Number of numbers is required")]
 		[Range(0, 100, ErrorMessage = "Number of numbers should be between 0 and 100.")]
 		public int NumberOfNumbers { get => numberOfNumbers; set { numberOfNumbers = value; OnPropertyChanged(nameof(NumberOfNumbers)); } }
@@ -37,7 +60,11 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 
 		public bool IncludeMarkSymbols { get => includeMarkSymbols; set { includeMarkSymbols = value; OnPropertyChanged(nameof(IncludeMarkSymbols)); } }
 
-		public string CustomSpecialCharacters { get => customSpecialCharacters; set { customSpecialCharacters = value; OnPropertyChanged(nameof(IncludeMarkSymbols)); } }
+		public string CustomSpecialCharacters { get => customSpecialCharacters; set { customSpecialCharacters = value; OnPropertyChanged(nameof(CustomSpecialCharacters)); } }
+
+		[Required(ErrorMessage = "Minimum length is required")]
+		[Range(1, 20, ErrorMessage = "Minimum length should be between 1 and 20.")]
+		public int MinimumLength { get => minimumLength; set { minimumLength = value; OnPropertyChanged(nameof(MinimumLength)); } }
 
 		public bool IsRandomStrokeOrder { get => isRandomStrokeOrder; set { isRandomStrokeOrder = value; OnPropertyChanged(nameof(IsRandomStrokeOrder)); } }
 
@@ -51,7 +78,8 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 
 		public KeystrokeOrder KeystrokeOrder
 		{
-			get {
+			get
+			{
 				if (IsRandomStrokeOrder) return KeystrokeOrder.Random;
 				if (IsAlternatingStrokeOrder) return KeystrokeOrder.AlternatingStroke;
 				if (IsAlternatingWordKeystroke) return KeystrokeOrder.AlternatingWord;
@@ -60,7 +88,8 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 				return KeystrokeOrder.Random;
 			}
 
-			set {
+			set
+			{
 				IsRandomStrokeOrder = value == KeystrokeOrder.Random;
 				IsAlternatingStrokeOrder = value == KeystrokeOrder.AlternatingStroke;
 				IsAlternatingWordKeystroke = value == KeystrokeOrder.AlternatingWord;
@@ -68,6 +97,7 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 				IsOnlyRightKeystrokes = value == KeystrokeOrder.OnlyRight;
 			}
 		}
+		#endregion
 
 		public bool HasChanges { get; set; } = false;
 
@@ -76,6 +106,15 @@ namespace PG.Wasm.PasswordGenerator.Components.Passwords.Models
 		{
 			HasChanges = true;
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public static ValidationResult ValidateDepthLowerThanAverage(object _, ValidationContext validationContext)
+		{
+			var options = validationContext.ObjectInstance as PasswordOptions;
+			if (options is not null && options.DepthLevel < options.AverageWordLength)
+				return ValidationResult.Success!;
+
+			return new ValidationResult("Depth level must be lower than the average word length");
 		}
 	}
 }
