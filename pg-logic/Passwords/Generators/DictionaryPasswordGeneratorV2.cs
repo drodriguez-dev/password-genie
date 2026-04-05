9fc9cf6b13070a44aef363a129d069129cb12ca8
drodriguez <drodriguez@esdelogica.es>
2026-04-05 14:30:40 +0200

    pg-logic\Passwords\Generators\DictionaryPasswordGeneratorV2 - Refactor recursive word generation state handling.

--- pg-logic/Passwords/Generators/DictionaryPasswordGeneratorV2.cs
+++ pg-logic/Passwords/Generators/DictionaryPasswordGeneratorV2.cs
@@ -7,6 +7,14 @@ namespace PG.Logic.Passwords.Generators
 	public class DictionaryPasswordGeneratorV2(DictionaryPasswordGeneratorOptions options, RandomService random, WordDictionaryTree wordTree)
 		: DictionaryPasswordGeneratorBase(options, random, wordTree)
 	{
+		private record struct WordGenerationResult(string Word, int Combinations);
+
+		private class WordGenerationState
+		{
+			public HandSide Hand { get; set; }
+			public Finger? Finger { get; set; }
+			public int Combinations { get; set; }
+		}
 
 		/// <summary>
 		/// Generates a word based on the dictionary provided. Word length is variable depending on the average word length, it's variance half of the 
@@ -17,65 +25,58 @@ namespace PG.Logic.Passwords.Generators
 		/// </remarks>
 		protected override string GenerateWord(int wordLength, int depthLevel, ref HandSide currentHand)
 		{
-			Finger? startFinger = null;
-			if (!IsChildrenPossible(string.Empty, wordLength, depthLevel, ref currentHand, ref startFinger, out string word, currentCombinations: 1, finalCombinations: out int finalCombinations))
+			WordGenerationState initialState = new() { Hand = currentHand, Finger = null, Combinations = 1 };
+			if (!TryGenerateWord(string.Empty, wordLength, depthLevel, initialState, output: out WordGenerationResult result))
 				throw new InvalidOperationException("Unable to generate a valid word. Please, try to reduce the restrictions.");
 
-			// Because method "IsChildrenPossible" is recursive, only the last call to the method will have the correct number of possibilities.
+			// Because method "TryGenerateWord" is recursive, only the last call to the method will have the correct number of possibilities.
 			_random.DiscardEntropy();
-			_random.IncrementEntropy(finalCombinations);
+			_random.IncrementEntropy(result.Combinations);
 			_random.CommitEntropy();
 
-			return CapitalizeWord(word);
+			return CapitalizeWord(result.Word);
 		}
 
-		private bool IsChildrenPossible(string word, int length, int depthLevel, ref HandSide hand, ref Finger? finger, out string newWord, int currentCombinations, out int finalCombinations)
+		private bool TryGenerateWord(string word, int length, int depthLevel, WordGenerationState state, out WordGenerationResult output)
 		{
 			if (length == 0)
 			{
-				newWord = word;
-				finalCombinations = currentCombinations;
+				output = new WordGenerationResult(word, state.Combinations);
 				return true; // Recursion exit condition, no more characters to add.
 			}
 
 			// Start node is the root node or the last node found in the tree up to depth level that matches the current word.
 			ITreeNode<string> startNode = FindLastPossibleLeafNode(word, depthLevel);
 
-			// Local variables to avoid "Cannot use ref parameter inside a lambda expression".
-			HandSide localHand = hand;
-			Finger? localFinger = finger;
-
 			var validChildren = startNode.Children
 				.Select(c => c.Value)
 				.Where(tn => !RemoveHighAsciiCharacters || tn.Value[0] < 128)
-				.Where(tn => IsProperHand(tn.Value[0], localHand))
-				.Where(tn => IsProperFinger(tn.Value[0], localHand, localFinger))
+				.Where(tn => IsProperHand(tn.Value[0], state.Hand))
+				.Where(tn => IsProperFinger(tn.Value[0], state.Hand, state.Finger))
 				.Select(tn => (
 					Node: tn,
-					IsPossible: IsChildrenPossible(word + tn.Value, length - 1, depthLevel, ref localHand, ref localFinger, out string newWord, currentCombinations, out int finalCombos),
-					Word: newWord,
-					Combinations: finalCombos
+					IsPossible: TryGenerateWord(word + tn.Value, length - 1, depthLevel, state, out WordGenerationResult result),
+					result.Word,
+					result.Combinations
 				))
 				.Where(ptn => ptn.IsPossible)
 				.ToList();
 
 			if (validChildren.Count <= 0)
 			{
-				newWord = string.Empty;
-				finalCombinations = currentCombinations;
+				output = default;
 				return false;
 			}
 
 			var (node, _, selectedWord, selectedCombinations) = validChildren[_random.Next(validChildren.Count, updateEntropy: false)];
 			selectedCombinations *= validChildren.Count;
 
-			finger = GetFingerForKeystroke(node.Value);
+			state.Finger = GetFingerForKeystroke(node.Value);
 
 			if (_options.KeystrokeOrder == KeystrokeOrder.AlternatingStroke)
-				hand = hand == HandSide.Left ? HandSide.Right : HandSide.Left;
+				state.Hand = state.Hand == HandSide.Left ? HandSide.Right : HandSide.Left;
 
-			newWord = selectedWord;
-			finalCombinations = selectedCombinations;
+			output = new WordGenerationResult(selectedWord, selectedCombinations);
 			return true;
 		}
 	}
